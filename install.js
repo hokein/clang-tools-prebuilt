e6b685a304e865e03d68e5e610110195f9f5e479
Haojian Wu <hokein.wu@gmail.com>
2016-07-08 22:17:29 +0200

    ES6 style.

--- install.js
+++ install.js
@@ -1,24 +1,24 @@
 const download = require('./download.js')
 const path = require('path')
 const mkdirp = require('mkdirp')
-const exec = require('child_process').exec;
+const exec = require('child_process').exec
 
 const dest = path.join(__dirname, './prebuilt_binaries/')
 
 download({
   'revision': '274142',
   'platform': 'mac'
-}, function(err, file_path) {
-  mkdirp(dest, function(error) {
+}, (err, file_path) => {
+  mkdirp(dest, (error) => {
     if (error) {
-      console.log(error);
-      return;
+      console.log(error)
+      return
     }
-    console.log("unziping " + file_path);
-    var cmd = 'tar -xzf ' + file_path + ' -C ' + dest;
-    exec(cmd, function(error, stdout, stderr) {
+    console.log("unziping " + file_path)
+    var cmd = 'tar -xzf ' + file_path + ' -C ' + dest
+    exec(cmd, (error, stdout, stderr) => {
       if (error) {
-        console.log(stderr);
+        console.log(stderr)
         return
       }
     })
